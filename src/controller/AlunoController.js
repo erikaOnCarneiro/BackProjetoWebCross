d97c19f117a5672fcc6054f0ec0a1c65fed86f5b
Erika <erikha.onc@gmail.com>
2025-04-15 23:25:54 -0300

    'concluindo as camadas de aluno'

--- src/controller/AlunoController.js
+++ src/controller/AlunoController.js
@@ -2,7 +2,17 @@ import { Router } from 'express';
 
 const endpoints = Router();
 
-endpoints.get('/aluno/listar', async (req, resp) =>{
-    let registros = await listar();
-    resp.send(registros);
-})
+endpoints.get('/aluno/listar', async (req, resp) => {
+    try {
+        const registros = await listar();
+        if (registros.length === 0) {
+            return resp.status(404).send({ mensagem: 'Nenhum registro localizado.' });
+        }
+
+        return resp.status(200).send(registros);
+
+    } catch (error) {
+        console.error('Erro ao listar alunos: ', error);
+        return resp.status(500).send({ mensagem: 'Erro interno no registro ao listar alunos.' });
+    }
+});
--- src/repository/AlunoRapository.js
+++ src/repository/AlunoRapository.js
@@ -8,8 +8,8 @@ export async function listarAlunos(){
 }
 
 export async function buscarAlunoPorIdOuCpf(param) {
-    const comando = `SELECT * FROM tb_aluno WHERE id = ?`;
-    const [registros] = await connection.query(comando, [id]);
+    const comando = `SELECT * FROM tb_aluno WHERE id = ? OR cpf?`;
+    const [registros] = await connection.query(comando, [param, param]);
     return registros[0];
   }
 
@@ -17,10 +17,10 @@ export async function inserir(aluno){
     const comando = `INSERT INTO tb_aluno(nome, cpf, nascimento, genero, email, endereco, telefone, 
     origem, foto, observacoes, dataRegistro, statusAluno) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    const [info] = await connection.query(comando, [nome.aluno, cpf.aluno, nascimento.aluno, 
-                                                    genero.aluno, email.aluno, endereco.aluno, 
-                                                    telefone.aluno, origem.aluno, foto.aluno, 
-                                                    observacoes.aluno, dataRegistro.aluno, statusAluno.aluno])
+    const [info] = await connection.query(comando, [aluno.nome, aluno.cpf, aluno.nascimento, 
+                                                    aluno.genero, aluno.email, aluno.endereco, 
+                                                    aluno.telefone, aluno.origem, aluno.foto, 
+                                                    aluno.observacoes, aluno.dataRegistro, aluno.statusAluno])
     
     return info.inserirId;
 }
